6ad44159ac382042154557573c8d0bd4dd3ea09f
elenoko <elenoko@gmail.com>
2018-12-10 20:59:24 +0100

    Postgres setttings

--- pur_beurre/settings/travis.py
+++ pur_beurre/settings/travis.py
@@ -3,7 +3,7 @@ from . import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '',
+        'NAME': 'travis_ci_test',
         'USER': 'postgres',
         'PASSWORD': '',
         'HOST': '',
